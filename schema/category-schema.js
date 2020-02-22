ab3210179f6294e9a3fd02e3ca51b75a805f4784
mai <mai.yusuf28@yahoo.com>
2020-02-22 08:13:29 +0200

    write the two schema for product and category

---
+++ schema/category-schema.js
@@ -0,0 +1,43 @@
+
+
+'use strict';
+
+
+const mongoose = require('mongoose');
+// require the product schema 
+require('./product-schema.js')
+/**
+ * this is the schema for category 
+ * place where we handle the information and organized
+ */
+const category = mongoose.Schema({
+  name : { type: String, required: true},
+},{ toObject: { virtuals: true }, toJSON: { virtuals: true },
+  
+});
+
+/**
+ * this clarify where the two schema connect as one virtual schema
+ */
+category.virtual('the_all_product', {
+
+  ref: 'product',
+  localField: 'name',
+  foreignField: 'categoryType',
+  justOne: false,
+  
+});
+  
+/**
+   * @param { object} virtual_schema
+   */
+function join() {
+  try {
+    this.populate('the-all_product');
+  }
+  catch(err){ throw Error;}
+}
+category.pre('find', join);
+category.pre('findOne', join);
+
+module.exports = mongoose.model('category', category);
